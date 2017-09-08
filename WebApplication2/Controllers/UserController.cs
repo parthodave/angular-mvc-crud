36915e87091790170d5ddc98a9c665712ed079fc
Lenovo <Lenovo@DESKTOP-P4F0DAS>
2017-09-08 16:52:46 +0530

    Add project files.

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        [Route("create-user")]
+        // GET: User
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string pwd { get; set; }
+    }
+}
